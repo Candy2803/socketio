680404c9af35a4ed20f8624acc97725bab59b1dd
+ <+>
2025-03-13 10:34:51 +0300

    comit

--- socket.js
+++ socket.js
@@ -1,13 +1,10 @@
-// socket.js
 const socketIo = require("socket.io");
 
 module.exports = (server) => {
-  // Initialize Socket.io with your server instance
   const io = socketIo(server, {
     cors: { origin: "*", methods: ["GET", "POST"] }
   });
 
-  // In-memory storage for messages (consider a persistent store for production)
   let messages = [];
 
   io.on("connection", (socket) => {
@@ -16,7 +13,6 @@ module.exports = (server) => {
     // Send existing messages to the connected client
     socket.emit("existingMessages", messages);
 
-    // Listen for incoming messages
     socket.on("sendMessage", (message) => {
       console.log("Received message:", message);
       messages.push(message);
